45b48c49acf0d360215ef6eb6ed0acbd2429873c
J Cole Morrison <j.cole.morrison@gmail.com>
2017-01-16 18:23:45 -0800

    full tutorial codebase

---
+++ app/src/components/widget/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Widget = () => {
+  return (
+    <div className='widget'>Yay we have SASS AND CRA AND STORYBOOK AND YARN!!!!</div>
+  )
+}
+
+export default Widget
